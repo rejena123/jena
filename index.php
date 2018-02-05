da29fdde890cfbd82b6e07cd11d235d2513ae64c
rejena123 <36025270+rejena123@users.noreply.github.com>
2018-02-05 10:15:36 +0800

    add new file index.php

--- index.php
+++ index.php
@@ -1,10 +1,68 @@
-<html>
-<head>
-<title>
-Hello
-</title>
-</head>
-<body>
-<p>Hello World</p>
+<!DOCTYPE.html>
+<body style="background-color:grey">
+<h1 style="color:skyblue">
+<center><h1>BIOGRAPHY</h1></center>
+<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My name is Rejena Grace Soncados Pitogo , 18 yrs. of young. I am the daughter of Josefina Soncados Pitogo and Victor Brignas Pitogo. I am currently living in Sitio , Riverside , Pangdan , City of Naga. My siblings are Lhem Jane Pitogo Malazarte and Maria Lovena Pitogo Gamol.</p></h3>
+<p></p>
+<p></p>
+<p></p>
+<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;When i was still young , I used to help my parents in our small business but now they are working as a production worker at OFFALS Department at Cebu Sherilin Industrial Corporation. As of now , i used to help my sister in her small business and taking care of her 2 child since im in my secondary school level.</p></h3>
+<p></p>
+<p></p>
+<p></p>
+<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;In my Preschool and Elementary level , I used to school in Tabok Elementary School I . In my Secondary level I used to school in Tabok National Highschool (1st yr.-2nd yr.) , I transfered in Don Emilio Canonigo National Highschool (3rd yr.-4th yr.). The reason why I transfered here its because the work of my family is here. In my Trinary level , I used to school in CTU-Naga Extension Campus.</p></h3> 
+<p></p>
+<p></p>
+<p></p>
+<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My goal in life is to be succesful in the field i have choosen. i want to help my parents . I want them to stop working and let them stay in the house because they are too old to work from the company where they are working now. I want them to take enough rest , thats why im working hard because my family is my inspiration , my love ones and they are my life because without them i cant be here.</p></h3>
+<h1 style="color:pink">
+<center><h1>PERSONAL INFORMATION</h1></center>
+<p><h3>Name:&nbsp;&nbsp;&nbsp;<u> Pitogo , Rejena Grace Soncados</u></h3></p>
+<p><h3>Address:&nbsp;&nbsp;<u> Pangdan,City of Naga , Cebu</u></h3></p>
+<p><h3>Age:&nbsp;&nbsp;&nbsp;<u> 18 years old</u></h3></p>
+<p><h3>Gender:&nbsp;&nbsp;&nbsp;<u> Female</u></h3></p>
+<p><h3>Email:&nbsp;&nbsp;&nbsp;<u> rejena213@gmail.com</u></h3></p>
+<p><h3>Date of Birth: &nbsp;<u> February 13 , 1999</u></h3></p>
+<p><h3>Place of Birth:&nbsp;<u> Tabok , Mandaue city</u></h3></p>
+<p><h3>Mother's Name: &nbsp;<u> Josefina s. Pitogo</u></h3></p>
+<p><h3>Father's Name: &nbsp;<u> Victor B. Pitogo</u></h3></p>
+<p><h3>Motto:&nbsp;&nbsp;&nbsp;<u> "The woman who follows the crowd will usually go no further than the crowd. The woman who walks alone is likely to find herself in places no one has been before."</u></h3></p>
+<hr><b><h1> EDUCATIONAL ATTAINMENT </h1></b></hr>
+
+<table border="1">
+	<tr>
+		<th>Level:</th>
+		<th>School:</th>
+		<th>Address:</th>
+		<th>Academic Year:</th>
+	</tr>
+	<tr>
+		<td>Elementary</td>
+		<td>Tabok Elementary School I</td>
+		<td>Tabok , Mandaue City</td>
+		<td>2010-2011</td>
+	</tr>
+	<tr>
+		<td>High School</td>
+		<td>Tabok National High School</td>
+		<td>Tabok Mandaue City</td>
+		<td>2011-2013</td>
+        </tr>
+	<tr>
+                <td></td>
+                <td>Don Emilio Canonigo Memorial High School</td>
+		<td>Pangdan , City of Naga , Cebu</td>
+		<td>2013-2015</td>           
+	</tr>
+	<tr>
+		<td>College</td>
+		<td> Cebu Technological University-Naga Extension Campus</td>
+		<td>CentralPoblacion,City of Naga</td>
+		<td>2017-present</td>
+	</tr>
+	
+</table>
+
 </body>
 </html>
+
